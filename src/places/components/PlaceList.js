55465ab4cc60b6cac7956063656909e0d5aaef95
dcampbellcreative <dougcampbell1990@gmail.com>
2022-04-11 22:16:42 -0400

    add logic for displaying loading screen, adjust custom hook

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,29 +1,35 @@
 import React from "react";
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 import { PlaceItem } from "./PlaceItem";
-import './PlaceList.css';
+import "./PlaceList.css";
 
 export const PlaceList = (props) => {
-	if (props.items.length === 0) {
-		return <div className="place-list center">
-			<Card>
-				<h2>No places found. Create one?</h2>
-				<button>Share Place</button>
-			</Card>
-		</div>
-	}
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. Create one?</h2>
+          <Button to="/places/new">Share Place</Button>
+        </Card>
+      </div>
+    );
+  }
 
-	return <ul className="place-list">
-		{props.items.map(place =>( 
-		<PlaceItem 
-		key={place.id} 
-		id={place.id} 
-		image={place.imageUrl} 
-		title={place.title} 
-		description={place.description}
-		address={place.address}
-		creatorID={place.creator}
-		coordinates={place.location}
-		/>))}
-	</ul>
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorID={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
 };
